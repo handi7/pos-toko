811d794f1dc5e7d615dc73f0aa6385c94e1d841c
handi7 <handi7.co@gmail.com>
2023-02-28 14:14:51 +0700

    Setup redux

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Dashboard() {
+  const user = useSelector((state) => state.user);
+
   return <div className="font-bold p-3">Dashboard</div>;
 }
