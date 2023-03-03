9d0fac627167df4cd895ecfe444cc0c132b2ab7d
handi7 <handi7.co@gmail.com>
2023-03-03 15:45:35 +0700

    Add error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,29 @@
+import { Space, Typography } from "antd";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const { Title } = Typography;
 
 export default function ErrorPage() {
-  return <div>not found</div>;
+  const navigate = useNavigate();
+
+  setTimeout(() => {
+    navigate("/");
+  }, 3000);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Space>
+        <Title level={3}>404</Title>
+        <Title level={5}>Page not Found.</Title>
+      </Space>
+    </div>
+  );
 }
